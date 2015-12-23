bd3a19cbe9ed1b4c19b24f11f6df888cb7cc1cc9
iceConfig <hlzheng7@gmail.com>
2015-12-23 14:54:08 +0800

    Hello Word

---
+++ HelloWord.js
@@ -0,0 +1,9 @@
+/**
+ * Created by iceConfig on 2015/12/23.
+ */
+var http = require("http");
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.end('Hello World\n');
+}).listen(8124,"127.0.0.1");
+console.log('Server running at http://127.0.0.1:8124/');
