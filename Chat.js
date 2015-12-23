3b1b9c341838d37d6b0c9b863e063c233f63fe46
iceConfig <hlzheng7@gmail.com>
2015-12-23 16:30:26 +0800

    Chat 2.0

--- Chat.js
+++ Chat.js
@@ -5,8 +5,12 @@ var net = require('net');
 var chatServer = net.createServer();
 chatServer.on('connection',function(client){
     client.write('Hi!\n');
-    client.write('Bye!\n');
-    client.end();
+
+    client.on('data',function(data){
+        console.log(data;)
+    })
+    //client.write('Bye!\n');
+    //client.end();
 });
 chatServer.listen(9000);
 console.log('Chat server started');
