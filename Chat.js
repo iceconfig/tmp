63aa2ddd436d7beea995e354944f03c34b7494a6
iceConfig <hlzheng7@gmail.com>
2015-12-23 17:13:21 +0800

    Chat 3.0

--- Chat.js
+++ Chat.js
@@ -3,14 +3,20 @@
  */
 var net = require('net');
 var chatServer = net.createServer();
-chatServer.on('connection',function(client){
-    client.write('Hi!\n');
-
-    client.on('data',function(data){
-        console.log(data);
+var clientList = [];
+chatServer.on('connection', function (client) {
+    client.name = client.remoteAddress + ':' + client.remotePort;
+    client.write('Hi! ' + client.name + '!\n');
+    clientList.push(client);
+    client.on('data', function (data) {
+        broadcast(data, client);
     })
-    //client.write('Bye!\n');
-    //client.end();
 });
+function broadcast(message, client) {
+    for (var i = 0; i < clientList.length; i++) {
+        if (client !== clientList[i])
+            clientList[i].write(client.name + ' says: ' + message);
+    }
+}
 chatServer.listen(9000);
 console.log('Chat server started');
